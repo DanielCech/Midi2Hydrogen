ba4d3ffa629d3131ed7ec97df7deafae35a15823
Daniel Čech <daniel.cech@gmail.com>
2024-03-20 22:04:49 +0100

    feat: conversion almost done

--- Midi2Hydrogen/Midi2Hydrogen/Midi2HydrogenApp.swift
+++ Midi2Hydrogen/Midi2Hydrogen/Midi2HydrogenApp.swift
@@ -7,20 +7,6 @@ import SwiftUI
 
 @main
 struct Midi2HydrogenApp: App {
-    init() {
-        #if os(iOS)
-            do {
-                Settings.bufferLength = .short
-                try AVAudioSession.sharedInstance().setPreferredIOBufferDuration(Settings.bufferLength.duration)
-                try AVAudioSession.sharedInstance().setCategory(.playAndRecord,
-                                                                options: [.defaultToSpeaker, .mixWithOthers, .allowBluetoothA2DP])
-                try AVAudioSession.sharedInstance().setActive(true)
-            } catch let err {
-                print(err)
-            }
-        #endif
-    }
-
     var body: some Scene {
         WindowGroup {
             MIDITrackDemo()
--- Midi2Hydrogen/Midi2HydrogenCommon/Sources/Midi2HydrogenCommon/Convertor.swift
+++ Midi2Hydrogen/Midi2HydrogenCommon/Sources/Midi2HydrogenCommon/Convertor.swift
@@ -12,12 +12,24 @@ import AudioKitEX
 import AudioKitUI
 import SwiftUI
 
+struct Note {
+    let position: Int
+    let velocity: Double
+    let instrument: Int
+}
+
+
 public class Convertor: ObservableObject {
 
     var hydrogenSong: XML?
 
+    /// magic number for resolution of hydrogen files
     let hydrogenResolution: Int = 48
+
+    /// number of beats per quarter note in midi file
     var midiResolution: Int = 0
+
+    /// number of beats per quarter in midi file / number of beats per quater in hydrogen
     var resolutionRatio: Double = 1
 
     public init() {
@@ -35,10 +47,30 @@ public class Convertor: ObservableObject {
             fatalError("MIDI file does not contain tracks")
         }
         
-        var tickCount = 0
+        var tickCount: Double = 0
+        var notes = [Note]()
 
         firstTrack.events.forEach { event in
+            guard 
+                let status = event.status,
+                let type = status.type,
+                let positionInBeats = event.positionInBeats
+            else {
+                return
+            }
 
+            if type == .noteOn {
+                tickCount += positionInBeats * Double(midiResolution)
+                notes.append(
+                    Note(
+                        position: Int(round(Double(tickCount) / resolutionRatio)),
+                        velocity: Double(event.data[2]) / 127.0,
+                        instrument: Int(event.data[1] - 36)
+                    )
+                )
+            } else if type == .noteOff {
+                tickCount += positionInBeats * Double(midiResolution)
+            }
         }
 
     }
