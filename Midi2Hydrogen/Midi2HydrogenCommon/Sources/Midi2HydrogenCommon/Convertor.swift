6068ba2cdfecc58351b74ed421a53ca8f8885a01
Daniel Čech <daniel.cech@gmail.com>
2024-03-20 17:27:57 +0100

    feat: scheme rename

--- Midi2Hydrogen/Midi2HydrogenCommon/Sources/Midi2HydrogenCommon/Convertor.swift
+++ Midi2Hydrogen/Midi2HydrogenCommon/Sources/Midi2HydrogenCommon/Convertor.swift
@@ -7,17 +7,40 @@
 
 import Foundation
 import SwiftyXML
+import AudioKit
+import AudioKitEX
+import AudioKitUI
+import SwiftUI
 
 public class Convertor: ObservableObject {
 
     var hydrogenSong: XML?
+
+    let hydrogenResolution: Int = 48
+    var midiResolution: Int = 0
+    var resolutionRatio: Double = 1
 
     public init() {
 
     }
 
     public func openFile(url: URL) {
-        // TODO
+        let midiFile = MIDIFile(url: url)
+        print(String(describing: midiFile))
+
+        midiResolution = Int(midiFile.timeDivision)
+        resolutionRatio = Double(midiResolution) / Double(hydrogenResolution)
+
+        guard let firstTrack = midiFile.tracks.first else {
+            fatalError("MIDI file does not contain tracks")
+        }
+        
+        var tickCount = 0
+
+        firstTrack.events.forEach { event in
+
+        }
+
     }
 
     public func openSong() {
